0b27e58b239b8e0db8025dfc29bd42554d191e06
Rajib <rajib@ubuntu>
2023-11-08 04:12:28 +0600

    cookie parser middleware added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
@@ -14,7 +15,7 @@ app.use(cors({
   credentials: true,
 }))
 app.use(express.json())
-
+app.use(cookieParser());
 
 
 
